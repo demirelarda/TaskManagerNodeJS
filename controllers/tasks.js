158bac30a1e35f090adff0a4ee47900d726d6aa0
Arda Demirel <yademirel21@gmail.com>
2023-05-12 21:39:51 +0300

    Initial Commit

---
+++ controllers/tasks.js
@@ -0,0 +1,30 @@
+
+
+const getAllTasks = (req,res)=>{
+    res.send('all items')
+}
+
+const getSingleTask = (req,res)=>{
+    res.json({id:req.params.id})
+}
+
+const createTask = (req,res)=>{
+    res.json(req.body)
+}
+
+const updateTask = (req,res)=>{
+    res.send('update task')
+}
+
+const deleteTask = (req,res)=>{
+    res.send('delete task')
+}
+
+
+module.exports = {
+    getAllTasks,
+    getSingleTask,
+    createTask,
+    updateTask,
+    deleteTask,
+}
